6080dffb18e24977bcfe38cce99843b747cb665e
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-26 18:43:21 +0800

    feat : create swiper instance for merchandise catalogue in landing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-//
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+window.Swiper = Swiper;
+window.SwiperModules = { Navigation, Pagination, Autoplay };
+
+const merchandiseSwiper = new Swiper("#merchandise-swiper", {
+    modules: [window.SwiperModules.Navigation],
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-next-btn",
+        prevEl: ".swiper-prev-btn",
+    },
+    breakpoints: {
+        320: { slidesPerView: 1, spaceBetween: 24 },
+        768: { slidesPerView: 2, spaceBetween: 24 },
+        1024: { slidesPerView: 3, spaceBetween: 32 },
+    },
+});
